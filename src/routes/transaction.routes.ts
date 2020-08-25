13703aad7ff3dbd0b8e1c3cf763dabf94ed807b3
phmtsweb <phmtsweb@gmail.com>
2020-08-25 00:08:58 -0300

    Adição codigo 201 ao método post

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -33,7 +33,7 @@ transactionRouter.post('/', (request, response) => {
       value,
       type,
     });
-    return response.json(transaction);
+    return response.status(201).json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
